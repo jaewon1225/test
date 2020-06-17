91c089a5e5f9b4416042fc154506e11bd7957bf7
jaewon1225 <58664211+jaewon1225@users.noreply.github.com>
2020-06-18 02:28:06 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,41 +1,27 @@
 <?php
-$url = 'https://storage.cloud.google.com/image-trans-12/test.jpg';
+$api_key = 'AIzaSyDVGM-H4bVy9eRPnTSz0wPB_82eXhbbqW0';
+$text = 'I love heesu';
+$source="en";
+$target="ko";
  
-$output_dir = "uploads/";
-if(isset($_FILES["file"]))
+$url = 'https://www.googleapis.com/language/translate/v2?key=' . $api_key . '&q=' . rawurlencode($text);
+$url .= '&target='.$target;
+$url .= '&source='.$source;
+ 
+$response = file_get_contents($url);
+$obj =json_decode($response,true); //true converts stdClass to associative array.
+if($obj != null)
 {
- 
-    $fileName = md5(date('Y-m-d H:i:s:u')).$_FILES["file"]["name"]; //unique filename
- 
-    //move the file to uploads folder
-    move_uploaded_file($_FILES["file"]["tmp_name"],$output_dir.$fileName);
-     
-     
-    //multipart form post using CURL
-    $filePath = realpath($output_dir.$fileName);
-    $post = array('apikey' => 'AIzaSyDVGM-H4bVy9eRPnTSz0wPB_82eXhbbqW0',
-                    'mode' => 'document_photo',
-                    'file' =>'@'.$filePath);
-         
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,$url);
-    curl_setopt($ch, CURLOPT_POST,1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-    $result=curl_exec ($ch);
-    curl_close ($ch);
-    echo $result;
- 
-    /*
-    //If you want to return only text use this.
-    $json = json_decode($result,true);
-    if($json && isset($json['text_block']))
+    if(isset($obj['error']))
     {
-        $textblock =$json['text_block'][0];
-        echo $textblock['text'];
-    }*/
-     
-    //remove the file
-    unlink($filePath);
- }
+        echo "Error is : ".$obj['error']['message'];
+    }
+    else
+    {
+        echo "Translsated Text: ".$obj['data']['translations'][0]['translatedText'];
+    }
+}
+else
+    echo "UNKNOW ERROR";
+ 
 ?>
