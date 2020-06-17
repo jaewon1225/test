28db1c4a7cba3464cfcd5841cf28c88957dec9cd
jaewon1225 <58664211+jaewon1225@users.noreply.github.com>
2020-06-18 01:32:26 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -1,32 +1,41 @@
 <?php
-namespace Google\Cloud\Samples\Vision;
-
-use Google\Cloud\Vision\V1\ImageAnnotatorClient;
-
-$path = './test.jpg';
-
-function detect_text($path)
+$url = 'https://api.idolondemand.com/1/api/sync/ocrdocument/v1';
+ 
+$output_dir = "uploads/";
+if(isset($_FILES["file"]))
 {
-    $imageAnnotator = new ImageAnnotatorClient();
-
-    # annotate the image
-    $image = file_get_contents($path);
-    $response = $imageAnnotator->textDetection($image);
-    $texts = $response->getTextAnnotations();
-
-    printf('%d texts found:' . PHP_EOL, count($texts));
-    foreach ($texts as $text) {
-        print($text->getDescription() . PHP_EOL);
-
-        # get bounds
-        $vertices = $text->getBoundingPoly()->getVertices();
-        $bounds = [];
-        foreach ($vertices as $vertex) {
-            $bounds[] = sprintf('(%d,%d)', $vertex->getX(), $vertex->getY());
-        }
-        print('Bounds: ' . join(', ', $bounds) . PHP_EOL);
-    }
-
-    $imageAnnotator->close();
-}
+ 
+    $fileName = md5(date('Y-m-d H:i:s:u')).$_FILES["file"]["name"]; //unique filename
+ 
+    //move the file to uploads folder
+    move_uploaded_file($_FILES["file"]["tmp_name"],$output_dir.$fileName);
+     
+     
+    //multipart form post using CURL
+    $filePath = realpath($output_dir.$fileName);
+    $post = array('apikey' => '4bc13566062bf3ea7c44daeea7b5c92ddde06f2d',
+                    'mode' => 'document_photo',
+                    'file' =>'@'.$filePath);
+         
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_POST,1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    $result=curl_exec ($ch);
+    curl_close ($ch);
+    echo $result;
+ 
+    /*
+    //If you want to return only text use this.
+    $json = json_decode($result,true);
+    if($json && isset($json['text_block']))
+    {
+        $textblock =$json['text_block'][0];
+        echo $textblock['text'];
+    }*/
+     
+    //remove the file
+    unlink($filePath);
+ }
 ?>
